23df00e3ca20dc85f74c5698f94248d4db397faf
Orionizer <gabriel.urbas@gmail.com>
2026-09-02 13:30:04 -0400

    Added player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 Move = new Vector3(x, y, 0f);
+        transform.Translate(Move * moveSpeed * Time.deltaTime);
+        }
+    }
+
